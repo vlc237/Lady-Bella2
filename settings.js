d099df9f79cc7457e0cc2890665985cfc95f945c
SNOWBIRD XMD <praiseluke47@gmail.com>
2025-07-20 23:37:11 +0200

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-  packname: '𝗕𝗘𝗡 10 𝗠𝗗',
+  packname: '',
   author: '‎',
-  botName: "𝗕𝗘𝗡 10 𝗠𝗗",
+  botName: ",
   botOwner: '103', // Your name
   ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
