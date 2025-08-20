40d138de4249f0a5eef4a3e81d4886a4db126d29
vlc237 <martineznguedjieu@gmail.com>
2025-08-20 07:52:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'lady bella',
   author: 'snowbird',
   botName: "Lady Bella",
-  botOwner: 'Snowbird', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol.
+  botOwner: '¥*ɿuɘƚɒ|ɘvɘЯ*¥', // Your name
+  ownerNumber: '237693383037', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
